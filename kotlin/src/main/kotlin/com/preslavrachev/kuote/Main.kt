ee609adc96a5aa2fffcb8c9d9c84609cf956d34a
Preslav Rachev <rachev.preslav@gmail.com>
2018-12-24 21:55:10 +0200

    Introduces type-safe routing via locations
    - via https://ktor.io/servers/features/locations.html

--- kotlin/src/main/kotlin/com/preslavrachev/kuote/Main.kt
+++ kotlin/src/main/kotlin/com/preslavrachev/kuote/Main.kt
@@ -4,14 +4,27 @@ import com.tlogx.ktor.pebble.Pebble
 import com.tlogx.ktor.pebble.PebbleContent
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
+import io.ktor.locations.KtorExperimentalLocationsAPI
+import io.ktor.locations.Location
+import io.ktor.locations.Locations
+import io.ktor.locations.get
 import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
+@Location("/k/{id}")
+class KuoteResource(val id: String)
+
+@KtorExperimentalLocationsAPI
 fun main(args: Array<String>) {
     embeddedServer(Netty, 8080) {
+
+        install(Locations)
+
         install(Pebble) {
             templateDir = "templates/"
         }
