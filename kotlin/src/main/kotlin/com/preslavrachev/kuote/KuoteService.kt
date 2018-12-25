44f32434f4a740afc996fd0ddb462fd684332ca7
Preslav Rachev <rachev.preslav@gmail.com>
2018-12-25 18:28:52 +0200

    Provides more thorough exception information

--- kotlin/src/main/kotlin/com/preslavrachev/kuote/KuoteService.kt
+++ kotlin/src/main/kotlin/com/preslavrachev/kuote/KuoteService.kt
@@ -9,6 +9,6 @@ data class Kuote(val objectID: String, val content: String)
 class KuoteService(val index: Index<Kuote>) {
     fun retrieveKuote(id: String): Kuote {
         val kuote = index.getObject("nytimes.com:the_myth_of_quality_time_3cd46d1a")
-        return kuote.orElseThrow()
+        return kuote.orElseThrow { RuntimeException("The kuote with ID: $id is not in the storage!") }
     }
 }
